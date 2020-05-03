654170ee283808f4d420dd70cbc0491cdebca94e
Blessen Alex <blessenalex009@gmail.com>
2020-05-03 19:00:55 +0530

    cleaning up code

--- lib/India.dart
+++ lib/India.dart
@@ -9,9 +9,7 @@ class India extends StatefulWidget {
 
 class _IndiaState extends State<India> {
   final String url = "https://api.covid19india.org/data.json";
-  final String url1 =
-      "https://api.covid19india.org/v2/state_district_wise.json";
-  List districtData;
+
   List indiaData;
   bool isLoading = true;
 
@@ -19,10 +17,8 @@ class _IndiaState extends State<India> {
   void initState() {
     super.initState();
     this.getJsonData();
-    this.getJsonDistrictData();
   }
 
-  //country data
   Future getJsonData() async {
     var response = await http.get(Uri.encodeFull(url));
     //  print(response.body);
@@ -33,27 +29,10 @@ class _IndiaState extends State<India> {
       indiaData = converttojson;
       isLoading = false;
     });
-    //print("INDIA DATA: $indiaData");
+    
   }
 
-  //District data
-  Future getJsonDistrictData() async {
-    var response = await http.get(Uri.encodeFull(url1));
-    //  print(response.body);
-    List converttojson1 = json.decode(response.body);
-
-    // print("OUR List: $converttojson1");
-    // var data = converttojson1[0];
-    // print(data);
-
-    setState(() {
-      districtData = converttojson1;
-      isLoading = false;
-    });
-    // print("DISTRICT DATA: $districtData");
-    var data = districtData[2];
-    print(data);
-  }
+  
 
   @override
   Widget build(BuildContext context) {
